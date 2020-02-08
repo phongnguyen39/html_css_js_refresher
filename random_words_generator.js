bdae0a8de8721c6bd776d3f07ef1b431a15f54be
phongnguyen39 <phong.nguyen39@gmail.com>
2020-02-07 22:36:58 -0800

    playing with HTML DOM methods

--- random_words_generator.js
+++ random_words_generator.js
@@ -1,7 +1,21 @@
 
-    document.getElementById('randomizer').innerHTML = "phong";
 
 
-    let d = new Date();
-    console.log(d);
-    document.getElementById('date_time').innerHTML = d;
+
+let d = new Date();
+console.log(d);
+
+function repeater() {
+    let repeat_output ='';
+    
+    for(let i = 0; i < 10; i++) {
+        
+        document.getElementById('repeater').innerHTML = repeat_output+=repeat_output = '<div class = grid_items>' + i +'</div>';
+    }
+    return repeat_output;
+}
+repeater();
+
+
+
+document.getElementById('date_time').innerHTML = d;
