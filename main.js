72fab281758de2ae5690dc6bf0dbc106215b95d4
phongnguyen39 <phong.nguyen39@gmail.com>
2020-02-09 07:26:42 -0800

    done with the refresher

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import {allElements} from '../periodic-table'
+// import {allElements} from '../periodic-table'
 
-window.alert(allElements[0])
+// window.alert(allElements[0])
+
 
 let d = new Date();
 console.log(d);
@@ -9,7 +10,10 @@ function repeater() {
     let repeat_output ='';
     
     for(let i = 0; i < 10; i++) {
-        document.getElementById('repeater').innerHTML = repeat_output+=repeat_output = '<div class = grid_items><h1>' + i +'</h1></div>';
+        document.getElementById('repeater').innerHTML =repeat_output+=repeat_output = '<div class = grid_items><h1>' + i +'</h1>' 
+        +'<button action="https://www.google.com"/>Range</button>'+
+        '</div>';
+
     }
     return repeat_output;
 }
--- period_table.mjs
+++ period_table.mjs
@@ -1,11 +1,11 @@
 // https://www.npmjs.com/package/periodic-table
 
-let pt = require('periodic-table');
+// let pt = require('periodic-table');
 
-let allElements = pt.all();
+// let allElements = pt.all();
 
-for(let i = 0; i< allElements.length; i++){
-    console.log(allElements[i].name)
-}
+// for(let i = 0; i< allElements.length; i++){
+//     console.log(allElements[i].name)
+// }
 
-export const allElements;
+// export const allElements;
